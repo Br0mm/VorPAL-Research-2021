a255d6f1920f16c677df281f14b776df1f5956a0
Br0mm <Andrew-Ost@mail.ru>
2021-02-21 22:41:21 +0300

    mini fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,7 +14,7 @@ fun main(args: Array<String>) {
             "${metrics.averageImplementationDepth}", "${metrics.maxImplementationDepth}", "${metrics.counterA}",
                 "${metrics.counterB}", "${metrics.counterC}")
     generator.generateXMLFile(args[1], args[0], metricValues)
-    println("Среднее количество переписанных методов для одного класса: " + metrics.averageOverriddenMethodsPerClass)
+    println("Среднее количество переопределённых методов для одного класса: " + metrics.averageOverriddenMethodsPerClass)
     println("Среднее количество полей для одного класса: " + metrics.averageFieldsPerClass)
     println("Средняя глубина наследования: " + metrics.averageImplementationDepth)
     println("Максимальная глубина наследования: " + metrics.maxImplementationDepth)
